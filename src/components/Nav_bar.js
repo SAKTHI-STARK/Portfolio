c3dc83090b0b72566d194c3b032117e94d41657e
SAKTHI-STARK <sakthi492003@gmail.com>
2024-10-31 23:46:05 +0530

    adding responsiveness

--- src/components/Nav_bar.js
+++ src/components/Nav_bar.js
@@ -5,13 +5,24 @@ import Header from './Header';
 import About from './About';
 import Contact from './Contact';
 import Viewwork from './View_work';
+import React, { useState } from 'react';
+
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false); // State to manage menu visibility
+    const toggleMenu = () => {
+        setIsOpen(!isOpen)
+        console.log(isOpen) // Toggle the menu open/close state
+    };
+
   return (
     <div>
       <nav className="navbar">
         <li id='name'>SAKTHI-STARK</li>
-        <ul>
+        <button className="menu-toggle" onClick={toggleMenu}>
+                &#9776; {/* Hamburger icon */}
+            </button>
+        <ul className={isOpen ? 'open' : ''}>
           <li><Link to="/">Home</Link></li>
           <li><Link to="https://github.com/SAKTHI-STARK" >Github</Link></li> 
           <li><Link to="https://www.linkedin.com/in/sakthivel-m-441522280?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">Linkedin</Link></li>
